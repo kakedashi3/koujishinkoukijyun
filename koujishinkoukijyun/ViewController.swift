e40b70d7853ec454c0127766d62e9b49a437bb12
tatsuoka <69132449+Pabulow2060@users.noreply.github.com>
2021-07-27 09:16:02 +0900

    Initial Commit

---
+++ koujishinkoukijyun/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  koujishinkoukijyun
+//
+//  Created by 立岡力 on 2021/07/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
