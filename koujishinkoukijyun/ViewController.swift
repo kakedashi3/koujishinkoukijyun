2d61c59e34a80ae5e0a8fe52dfacea8df39725d6
tatsuoka <69132449+Pabulow2060@users.noreply.github.com>
2021-07-29 06:51:28 +0900

    first commit！

--- koujishinkoukijyun/ViewController.swift
+++ koujishinkoukijyun/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var kessanKoujiGenkaTextField: UITextField!
+    @IBOutlet weak var sougakuKoujiGenkaTextField: UITextField!
+    @IBOutlet weak var koujiShintyokudo: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // 入力欄にヒント文字を表示する
+        kessanKoujiGenkaTextField.placeholder = "発生した工事原価"
+        sougakuKoujiGenkaTextField.placeholder = "工事原価総額"
+        // 数字入力用のキーボードに変換
+        self.kessanKoujiGenkaTextField.keyboardType = UIKeyboardType.numberPad
+        self.sougakuKoujiGenkaTextField.keyboardType = UIKeyboardType.numberPad
     }
-
-
+    
+    // 画面をタッチしてキーボードを閉じる
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+            self.view.endEditing(true)
+        }
+    
+    // ボタンを押下して工事進捗度を算出する
+    @IBAction func calculationButton(_ sender: Any) {
+        let doubleK = Double(kessanKoujiGenkaTextField.text!)
+        let doubleS = Double(sougakuKoujiGenkaTextField.text!)
+        koujiShintyokudo.text = calculation(kessan: doubleK!, sougaku: doubleS!)
+    }
+    
+    // 計算式で結果を出して値を返す
+    func calculation(kessan: Double, sougaku: Double) -> String {
+        let k = kessan
+        let s = sougaku
+        var result = k / s
+        result = floor(result * 10)
+        return result.description
+    }
+    
+    // ボタンを丸くする
+    @IBOutlet weak var curveButton: UIButton!
+    override func viewDidLayoutSubviews() {
+            super.viewDidLayoutSubviews()
+            // 画面上の座標設定
+            self.curveButton.frame = CGRect(x: (self.view.frame.size.width / 2) - 150,
+                                            y: (self.view.frame.size.height / 2) + 130, width: 300, height: 100)
+            //角丸の程度を指定
+            self.curveButton.layer.cornerRadius = 40.0
+        }
+    
+    
 }
 
